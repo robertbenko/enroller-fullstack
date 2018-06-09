4ef972de0d0c4bf6a28e0f8611cb4a9768c1bca4
student24 <student24@pcoip.ki.agh.edu.pl>
2018-06-09 13:55:48 +0200

    firt commit on classes

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -11,7 +11,15 @@
       <meetings-page :username="authenticatedUsername"></meetings-page>
     </div>
     <div v-else>
-      <login-form @login="login($event)"></login-form>
+      <button :class =" isRegistering ? 'button-outline' : ''"
+            @click="isRegistering = false">Zaloguj się</button>
+      <button :class =" !isRegistering ? 'button-outline' : ''"
+            @click="isRegistering = true">Zarejestruj się</button>
+
+      <login-form @login="login($event)"
+                  v-if="!isRegistering"></login-form>
+      <login-form @login="register($event)"
+                  v-else></login-form>
     </div>
   </div>
 </template>
@@ -25,7 +33,8 @@
         components: {LoginForm, MeetingsPage},
         data() {
             return {
-                authenticatedUsername: ""
+                authenticatedUsername: "",
+                isRegistering: false
             };
         },
         methods: {
